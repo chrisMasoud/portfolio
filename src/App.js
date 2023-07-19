e579aa30bcdf5bd0f28f4e1913f3568f43317002
chrisMasoud <chrismasoud22@gmail.com>
2023-07-19 15:35:56 -0400

    projects page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
       <Hero />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import img from "../img/SkateSpotter.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "../styles/Projects.css";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="projects">
+      <div className="container">
+        <div className="project-content">
+          <p>Projects</p>
+          <h3>Each project is a unique piece of development</h3>
+          <div className="projects-grid">
+            <div className="project-card">
+              <div className="project-img">
+                <img src={img} alt="SkateSpotter Image" />
+              </div>
+              <div className="project-text">
+                <h3>SkateSpotter</h3>
+                <p>
+                  SkateSpotter is a Full Stack Web App that lets skateboarders
+                  find and share local street skate spots. The website provides
+                  an interface for searching, adding, comparing, reviewing, and
+                  saving skate spots.
+                </p>
+                <div className="stack">
+                  <p>React</p>
+                  <p>CSS</p>
+                  <p>Node.js</p>
+                </div>
+                <div className="links">
+                  <a
+                    target="_blank"
+                    href="https://github.com/chrisMasoud/SkateSpotter"
+                    rel="noreferrer"
+                  >
+                    Code{" "}
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      size="2x"
+                      className="fa-icon"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
