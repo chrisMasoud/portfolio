5d176287b4425e73ed0f029fd5e2ed8def5c8274
chrisMasoud <chrismasoud22@gmail.com>
2023-07-19 17:55:06 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer">
+          <h3>Copyright © 2023. All rights reserved.</h3>
+          <div className="footer-socials">
+            <a
+              aria-label="linkedin"
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/chrismasoud/"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2x"
+                className="fa-icon"
+              />
+            </a>
+            <a
+              aria-label="github"
+              rel="noreferrer"
+              target="_blank"
+              href="https://github.com/chrisMasoud"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2x" className="fa-icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
